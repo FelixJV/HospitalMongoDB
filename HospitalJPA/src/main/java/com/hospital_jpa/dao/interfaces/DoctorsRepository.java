c253bf931c65c56e1977778ebb3c53f9a74cbf2b
2dam <jimenezvelascofelix@gmail.com>
2025-02-12 13:54:38 +0100

    Creds Acabado

--- HospitalJPA/src/main/java/com/hospital_jpa/dao/interfaces/DoctorsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.hospital_jpa.dao.interfaces;
-
-import com.hospital_jpa.dao.model.Doctor;
-
-import java.util.List;
-
-public interface DoctorsRepository {
-    List<Doctor> getAll();
-}
